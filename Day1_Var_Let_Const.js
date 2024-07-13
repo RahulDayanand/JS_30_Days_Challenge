9623e1381a6b9a0b9da3698c6b6356defd3a881e
RahulDayanand <101518605+RahulDayanand@users.noreply.github.com>
2024-07-13 18:17:38 +0000

    var,let,const

---
+++ Day1_Var_Let_Const.js
@@ -0,0 +1,82 @@
+//Activity 1 - Variable Declaration
+
+var number = 5;
+let string = "Hello";
+console.log(number);
+console.log(string);
+
+//Activity 2 - Constant Declaration
+
+const Boolean = true;
+console.log(Boolean);
+
+//Activity 3 - Data Types
+
+let object = {firstName: "RahulD", age: 24, lastName: "D" };
+let array = ["Rahul", "Kiran", "Bindu", "Harry", "Pavan", "DP", 5];
+
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof Boolean);
+console.log(typeof object);
+console.log(typeof array);
+console.log(typeof null);
+
+//Activity 4 - Reassigning Variables
+
+let n = 5;
+console.log(n);
+
+n = 6;
+console.log(n);
+
+//Activity 5 - Understanding Const
+
+const a = 7;
+//a = 8;
+//console.log(a);
+
+//Feature Request
+
+//1. Data Types And TypeOf
+
+let num = 5;
+let word = "Hello";
+let Boo = false;
+let nu = null;
+let undefined;
+let symbol = "$";
+let objects = {};
+let arrays = [];
+
+console.log(num, typeof num);
+console.log(word, typeof word);
+console.log(Boo, typeof Boo);
+console.log(nu, typeof nu);
+console.log(undefined, typeof undefined);
+console.log(symbol, typeof symbol);
+console.log(objects, typeof objects);
+console.log(arrays, typeof arrays);
+
+//Reassignment Demo - Difference Between Let And Const
+
+//Let
+
+let s = "something";
+console.log(s);
+s = 5;
+console.log(s);
+//let s = "error"; //Can't redeclare in let but can be reassigned
+// console.log(s);
+
+//Const
+
+// const m; //have to do both declaration and initiliation,can't only be declared
+// m=5;
+// const m=6;
+// m=6;  //Can neither redeclare nor reassign the value
+
+
+
+
+
