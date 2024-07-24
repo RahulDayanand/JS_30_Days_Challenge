765d3ff5392a617435ce43fa732151e1bc8bdae1
RahulDayanand <101518605+RahulDayanand@users.noreply.github.com>
2024-07-24 06:59:47 +0000

    Function Declarations

---
+++ Day5_Functions/functions.js
@@ -0,0 +1,22 @@
+//Activity 1
+
+//Task 1
+
+function oddEven(n){
+    if(n % 2 === 0){
+        console.log('Even Number');
+    } else {
+        console.log('Odd Number');
+    }
+}
+
+oddEven(6);
+
+//Task 2
+
+function squareNumber(n){
+    let square = n * n;
+    console.log(square);
+}
+
+squareNumber(5);
